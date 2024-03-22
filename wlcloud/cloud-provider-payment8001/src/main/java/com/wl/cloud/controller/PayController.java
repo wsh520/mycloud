9a13a839ef67974053d13fc997aab615cdcac86a
不如归去 <wl16675181281@163.com>
2024-03-22 14:07:18 +0800

    feat(same response and exception): same response and exception
    add same response,and add global excepiton handle

--- wlcloud/cloud-provider-payment8001/src/main/java/com/wl/cloud/controller/PayController.java
+++ wlcloud/cloud-provider-payment8001/src/main/java/com/wl/cloud/controller/PayController.java
@@ -3,6 +3,7 @@ package com.wl.cloud.controller;
 
 import com.wl.cloud.DTO.PayDTO;
 import com.wl.cloud.entities.Pay;
+import com.wl.cloud.resp.ResultData;
 import com.wl.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,37 +23,38 @@ public class PayController {
 
     @PostMapping(value = "/add")
     @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public String addPay(@RequestBody Pay pay){
+    public ResultData<String> addPay(@RequestBody Pay pay){
         System.out.println(pay.toString());
         int i = payService.add(pay);
-        return "成功插入记录，返回值："+i;
+        return ResultData.success("成功插入记录，返回值："+i);
     }
 
     @DeleteMapping(value = "/del/{id}")
     @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        return ResultData.success(payService.delete(id));
     }
 
     @PutMapping(value = "/update")
     @Operation(summary = "修改",description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO payDTO){
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO){
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
 
         int i = payService.update(pay);
-        return "成功修改记录，返回值："+i;
+        return ResultData.success("成功修改记录，返回值："+i);
     }
 
     @GetMapping(value = "/get/{id}")
     @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
-    public Pay getById(@PathVariable("id") Integer id){
-        return payService.getById(id);
+    public ResultData<Pay> getById(@PathVariable("id") Integer id){
+        return ResultData.success(payService.getById(id));
     }
 
     @GetMapping(value = "/getAll")
     @Operation(summary = "查全部流水",description = "查询全部支付流水方法")
-    public List<Pay> getAll(){
-        return payService.getAll();
+    public ResultData<List<Pay>> getAll(){
+
+        return ResultData.success(payService.getAll());
     }
 }
