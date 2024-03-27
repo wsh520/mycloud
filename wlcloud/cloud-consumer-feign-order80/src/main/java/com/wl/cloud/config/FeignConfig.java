43814dca140a3ddfed94ceccf0049da122933fde
不如归去 <wl16675181281@163.com>
2024-03-27 16:46:23 +0800

    开启feign request 和response 压缩功能,并开启日志信息,开启日志信息后可以明显看到重试流程

--- wlcloud/cloud-consumer-feign-order80/src/main/java/com/wl/cloud/config/FeignConfig.java
+++ wlcloud/cloud-consumer-feign-order80/src/main/java/com/wl/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.wl.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,11 +8,27 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignConfig {
 
+    /**
+     * 设置feign 重试
+     * @return
+     */
     @Bean
     public Retryer myRetryer() {
 
         //最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
-//        return new Retryer.Default(100,1,3);
-        return Retryer.NEVER_RETRY; // 不使用重试
+        return new Retryer.Default(100,1,3);
+//        return Retryer.NEVER_RETRY; // 不使用重试
+    }
+
+
+    /**
+     * 设置feign 日志级别
+     *
+     * @return
+     */
+    @Bean
+    public Logger.Level feignLoggerLevel(){
+
+        return Logger.Level.FULL;
     }
 }
