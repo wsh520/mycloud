0a6cbc7c8863805f3e504dabcba3e743e85df98e
不如归去 <wl16675181281@163.com>
2024-04-19 10:35:38 +0800

    使用 ratelimiter 来进行限流

--- wlcloud/cloud-consumer-feign-order80/src/main/java/com/wl/cloud/controller/OrderCircuitController.java
+++ wlcloud/cloud-consumer-feign-order80/src/main/java/com/wl/cloud/controller/OrderCircuitController.java
@@ -3,6 +3,7 @@ package com.wl.cloud.controller;
 import com.wl.cloud.apis.PayFeignApi;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -67,4 +68,14 @@ public class OrderCircuitController
     }
 
 
+    @GetMapping(value = "/feign/pay/ratelimit/{id}")
+    @RateLimiter(name = "cloud-payment-service",fallbackMethod = "myRatelimitFallback")
+    public String myBulkheadRatelimit(@PathVariable("id") Integer id)
+    {
+        return payFeignApi.myRatelimit(id);
+    }
+    public String myRatelimitFallback(Integer id,Throwable t)
+    {
+        return "你被限流了，禁止访问/(ㄒoㄒ)/~~";
+    }
 }
