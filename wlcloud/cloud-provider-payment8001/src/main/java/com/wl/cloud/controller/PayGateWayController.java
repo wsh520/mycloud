40fa73483f79f8b96f096ee8c4d35733f34d5c23
不如归去 <wl16675181281@163.com>
2024-04-23 10:40:35 +0800

    8001服务前添加gateway网关,使用网关来访问服务信息

---
+++ wlcloud/cloud-provider-payment8001/src/main/java/com/wl/cloud/controller/PayGateWayController.java
@@ -0,0 +1,30 @@
+package com.wl.cloud.controller;
+
+import cn.hutool.core.util.IdUtil;
+import com.wl.cloud.entities.Pay;
+import com.wl.cloud.resp.ResultData;
+import com.wl.cloud.service.PayService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PayGateWayController
+{
+    @Resource
+    PayService payService;
+
+    @GetMapping(value = "/pay/gateway/get/{id}")
+    public ResultData<Pay> getById(@PathVariable("id") Integer id)
+    {
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
+    }
+
+    @GetMapping(value = "/pay/gateway/info")
+    public ResultData<String> getGatewayInfo()
+    {
+        return ResultData.success("gateway info test："+ IdUtil.simpleUUID());
+    }
+}
